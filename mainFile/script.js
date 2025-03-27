883db0bde9e846dfcb1d04c56b5afd98a9ff9799
Mayank pareta <mayankpareta999@gmail.com>
2025-03-27 12:06:36 +0530

    Add files via upload

---
+++ mainFile/script.js
@@ -0,0 +1,13 @@
+//Back To Top Button
+const scroll = document.querySelector("#btn-back");
+
+scroll.addEventListener("click", () => {
+     
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    
+   
+
+})
